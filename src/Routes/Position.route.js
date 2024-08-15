d81a6247ca78266bea4518514824afbb0a0dc946
Elias Valenzuela <elias.ariel.valenzuela@gmail.com>
2024-08-14 23:15:28 -0300

    bug fix on position controller import

--- src/Routes/Position.route.js
+++ src/Routes/Position.route.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getPositions } from '../Controllers/positionsController.js';
+import { getPositions } from '../Controllers/PositionsController.js';
 
 const positionRoutes = Router()
 
