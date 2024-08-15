0c2bb31cae07880858c8d7287824b855f996b1ff
Elias Valenzuela <elias.ariel.valenzuela@gmail.com>
2024-08-14 23:06:45 -0300

    configuration changed for deploy

---
+++ index.js
@@ -0,0 +1,4 @@
+import app from './src/app.js'
+import { PORT } from './src/config.js'
+
+app.listen(PORT)
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import app from './app.js'
-import { PORT } from './config.js'
-
-app.listen(PORT)
